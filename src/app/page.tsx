f98e19f6596323faa306c400b866fef4cab2b530
sierrabonita <m4mba@M4MBA.local>
2025-07-20 09:30:18 +0900

    Separate login form sample from top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,15 @@
 'use client'
 
-import {
-  Button,
-  Box,
-  Field,
-  Fieldset,
-  Flex,
-  Input,
-  VStack,
-} from '@chakra-ui/react'
+import { Flex, Link } from '@chakra-ui/react'
 
-export default function LoginPage() {
+export default function TopPage() {
   return (
     <Flex
       minH="100vh"
       align="center"
       justify="center"
     >
-      <Box
-        p={8}
-        rounded="xl"
-        shadow="lg"
-        w="full"
-        maxW="md"
-      >
-        <Fieldset.Root
-          size="lg"
-          maxW="md"
-        >
-          <VStack align={'flex-start'}>
-            <Fieldset.Legend>ログイン</Fieldset.Legend>
-            <Fieldset.HelperText>
-              メールアドレスとパスワードを設定してください
-            </Fieldset.HelperText>
-          </VStack>
-
-          <Fieldset.Content>
-            <Field.Root>
-              <Field.Label>メールアドレス</Field.Label>
-              <Input
-                name="email"
-                type="email"
-              />
-            </Field.Root>
-
-            <Field.Root>
-              <Field.Label>パスワード</Field.Label>
-              <Input
-                name="password"
-                type="password"
-              />
-            </Field.Root>
-          </Fieldset.Content>
-
-          <Button
-            type="submit"
-            alignSelf={'center'}
-          >
-            ログイン
-          </Button>
-        </Fieldset.Root>
-      </Box>
+      <Link href="/loginForm">loginForm</Link>
     </Flex>
   )
 }
